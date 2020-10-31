89a1d7632e78523c60f613565cd5495a504d1765
Benjamin Brown <benjamincwbrown@gmail.com>
2020-10-31 17:45:25 +0000

    finished referencing and finished contingency tables

--- make.sh
+++ make.sh
@@ -26,18 +26,18 @@ if [[ -d $BUILDDIR ]]; then
             echo "Error building $DOCUMENT.pdf"
             echo "See tex log ($BUILDDIR/$DOCUMENT.log) for more information."
         fi
-#        xelatex $DOCUMENT169.tex $PASS_ONE
-#        if [[ $? == 0 ]]; then
-#            biber $DOCUMENT169
-#            makeglossaries $DOCUMENT169
-#            echo "2nd pass ..."
-#            xelatex $DOCUMENT169.tex $PASSES > /dev/null
-#            cp $DOCUMENT169.pdf ../output/
-#        else
-#            clear
-#            echo "Error building $DOCUMENT169.pdf"
-#            echo "See tex log ($BUILDDIR/$DOCUMENT169.log) for more information."
-#        fi
+        xelatex $DOCUMENT169.tex $PASS_ONE
+        if [[ $? == 0 ]]; then
+            biber $DOCUMENT169
+            makeglossaries $DOCUMENT169
+            echo "2nd pass ..."
+            xelatex $DOCUMENT169.tex $PASSES > /dev/null
+            cp $DOCUMENT169.pdf ../output/
+        else
+            clear
+            echo "Error building $DOCUMENT169.pdf"
+            echo "See tex log ($BUILDDIR/$DOCUMENT169.log) for more information."
+        fi
     popd
 else
     echo "$BUILDDIR does not exist or is not accessible!"
